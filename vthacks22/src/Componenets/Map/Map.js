3e9a336baf69241f4271a4bcbbd9050854cb9bf4
pierretranva <pierretranva@gmail.com>
2022-11-13 00:44:03 -0500

    feat: updates to map component

--- vthacks22/src/Componenets/Map/Map.js
+++ vthacks22/src/Componenets/Map/Map.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import {Button} from '@mui/material'
 
 const containerStyle = {
-    width: '400px',
-    height: '400px'
+    width: '1000px',
+    height: '700px'
   };
   
   const center = {
-    lat: -3.745,
-    lng: -38.523
+    lat: 41,
+    lng: -99
   };
   
   function MyComponent() {
@@ -16,27 +17,50 @@ const containerStyle = {
       id: 'google-map-script',
       googleMapsApiKey: "AIzaSyAuculb2UY8iTySBcdEDQofpkGgBksbclA"
     })
-  
+    const [error, setError] = React.useState(null);
+    const [resultLoaded, setResultLoaded] = React.useState(false);
+    const [items, setItems] = React.useState([]);
     const [map, setMap] = React.useState(null)
   
     const onLoad = React.useCallback(function callback(map) {
-      // This is just an example of getting and using the map instance!!! don't just blindly copy!
-      const bounds = new window.google.maps.LatLngBounds(center);
-      map.fitBounds(bounds);
-  
-      setMap(map)
+      
     }, [])
   
     const onUnmount = React.useCallback(function callback(map) {
       setMap(null)
     }, [])
-  
+
+    React.useEffect(() => {
+      fetch("https://api.example.com/items")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setResultLoaded(true);
+            setItems(result);
+            ;
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          (error) => {
+            setResultLoaded(true);
+            setError(error);
+          }
+        )
+    }, [])
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (1==2) {
+      return <div>Loading...</div>;
+    }
+      else{
     return isLoaded ? (
         <div class= "main">
+        <Button variant="contained">Contained</Button>
         <GoogleMap
           mapContainerStyle={containerStyle}
           center={center}
-          zoom={10}
+          zoom={4.4}
           onLoad={onLoad}
           onUnmount={onUnmount}
         >
@@ -46,5 +70,6 @@ const containerStyle = {
         </div>
     ) : <></>
   }
+}
   
   export default React.memo(MyComponent)
